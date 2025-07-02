22f0fbaefdd688e831b244b07bc89bd7b9bd3c49
Xiushan Lu <125161791+xiushanlu@users.noreply.github.com>
2025-07-02 16:43:30 -0700

    Create basic hero section with counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  let [clickTimes, setClickTimes] = useState(0);
+
+  let updateClickTimes = () => {
+    setClickTimes(++clickTimes);
+  }
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-purple-500 to-pink-500 text-white text-4xl font-bold">
-      Tailwind is 🔥
+    <div className="w-screen bg-amber-50 h-screen flex flex-col p-10 gap-3">
+      <h1>Hi!</h1>
+      <p>This button was clicked {clickTimes} times since reload.</p>
+      <button onClick = {updateClickTimes}>Learn more</button>
     </div>
   )
 }
